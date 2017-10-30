c1fd2f29111f8d69dd5cb6532f7c3d1783c51fca
wangjiayan <wangjiayan@zcshow.com>
2017-10-30 16:37:51 +0800

    update

--- manage_sys/src/main.js
+++ manage_sys/src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 import {routerMode} from './config/env'
 import routes from './router'
 import Vuex from 'vuex'
