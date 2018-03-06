da03d10afc005009f46c225df060dd1325933a9c
Wangjiayan <wangjiayan@suadi.com.cn>
2018-03-06 13:51:27 +0800

    update

--- project/src/config/fetch.js
+++ project/src/config/fetch.js
@@ -8,6 +8,79 @@ axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded
 axios.defaults.baseURL = 'http://cangdu.org:8001';
 let baseUrl = 'http://cangdu.org:8001';
 
+// export default (url = '', data = {}, type = 'GET') =>{
+//   type = type.toUpperCase();
+//   url=baseUrl+url;
+//
+//   //处理参数
+//   let formData = new FormData();
+//   Object.keys(data).forEach(key => {
+//     formData.append(key,data[key])
+//   })
+//
+//   var obj = [];
+//
+//
+//   return new Promise((resolve, reject) => {
+//     if (type == 'GET') {
+//       // Spin.show();
+//       axios.get(url,{params: data}).then(response => {
+//         // let res = JSON.parse(Base64.decode(response.data));
+//         let res = response.data;
+//         // Spin.hide();
+//         resolve(res)
+//       }).catch(response => {
+//         reject(response)
+//       })
+//     }
+//     else if (type == 'POST') {
+//       // Spin.show();
+//       axios.post(url,formData).then(response => {
+//           // let res = JSON.parse(Base64.decode(response.data));
+//           let res = response.data;
+//           // Spin.hide();
+//           //需要responseCode == 1的情况
+//           if(noResponseCode){
+//             if(res.response_code == 1){
+//               obj =  res
+//               resolve(obj)
+//             }else{
+//               reject(res)
+//             }
+//           }
+//           //无需responseCode的情况
+//           else{
+//             obj =  res
+//             resolve(obj)
+//           }
+//
+//         })
+//         .catch(response => {
+//           reject(response)
+//         })
+//     }
+//   })
+//
+// }
+//
+
+function checkStatus(response,resolve,reject) {
+  if (response) {
+    // 如果状态正常，则直接返回数据
+   // if(response.code === 1 ){
+      resolve(response.data)
+   // }
+    // 异常状态下，把错误信息返回去
+    // else {
+    //   reject(response)
+    // }
+  }else{
+    reject(response)
+  }
+}
+
+
+
 export default (url = '', data = {}, type = 'GET') =>{
   type = type.toUpperCase();
   url=baseUrl+url;
@@ -25,10 +98,7 @@ export default (url = '', data = {}, type = 'GET') =>{
     if (type == 'GET') {
       // Spin.show();
       axios.get(url,{params: data}).then(response => {
-        // let res = JSON.parse(Base64.decode(response.data));
-        let res = response.data;
-        // Spin.hide();
-        resolve(res)
+        checkStatus(response,resolve,reject)
       }).catch(response => {
         reject(response)
       })
@@ -36,28 +106,10 @@ export default (url = '', data = {}, type = 'GET') =>{
     else if (type == 'POST') {
       // Spin.show();
       axios.post(url,formData).then(response => {
-          // let res = JSON.parse(Base64.decode(response.data));
-          let res = response.data;
-          // Spin.hide();
-          //需要responseCode == 1的情况
-          if(noResponseCode){
-            if(res.response_code == 1){
-              obj =  res
-              resolve(obj)
-            }else{
-              reject(res)
-            }
-          }
-          //无需responseCode的情况
-          else{
-            obj =  res
-            resolve(obj)
-          }
-
-        })
-        .catch(response => {
-          reject(response)
-        })
+        checkStatus(response,resolve,reject)
+      }).catch(response => {
+        reject(response)
+      })
     }
   })
 
